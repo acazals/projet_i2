7be026fc136072ad03dfbd1dffb78930d27e7af6
acazals <cazalsan@gmail.com>
2024-11-16 10:33:08 +0100

    page15, ici ca marche

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Character
 from .models import Equipement
 
@@ -6,4 +6,6 @@ from .models import Equipement
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    personnes = Character.objects.all()
+    #personne = get_object_or_404(Character, pk=pk)
+    return render(request, 'blog/post_list.html', {'personnes': personnes})
